b77936fc5bd68df27f98b7ef9a75c69cd7fc1986
yesitsmeagainhi <141656177+yesitsmeagainhi@users.noreply.github.com>
2025-11-04 12:18:27 +0530

    Remove redundant React import statement

--- src/components/FolderView.tsx
+++ src/components/FolderView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { listChildren, updateNode, deleteNode, recursiveDelete, type NodeDoc } from "../api/firestore";
 import classNames from "classnames";
 
